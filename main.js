ad9f99b674e82ab4c623cef662624dd76cfd3ca9
moclan110 <vulan11101997@gmail.com>
2022-11-09 08:24:21 +0700

    Ung dung may tinh

---
+++ main.js
@@ -0,0 +1,24 @@
+// function cong() {
+//     let a = +document.getElementById('number1').value;
+//     let b = +document.getElementById('number2').value;
+//     let result = a+b;
+//     document.getElementById('result').innerHTML = 'Kết quả là: ' + result
+// }
+// function tru() {
+//     let a = +document.getElementById('number1').value;
+//     let b = +document.getElementById('number2').value;
+//     let result = a-b;
+//     document.getElementById('result').innerHTML = 'Kết quả là: ' + result
+// }
+// function nhan() {
+//     let a = +document.getElementById('number1').value;
+//     let b = +document.getElementById('number2').value;
+//     let result = a*b;
+//     document.getElementById('result').innerHTML = 'Kết quả là: ' + result
+// }
+// function chia() {
+//     let a = +document.getElementById('number1').value;
+//     let b = +document.getElementById('number2').value;
+//     let result = a/b;
+//     document.getElementById('result').innerHTML = 'Kết quả là: ' + result
+// }
